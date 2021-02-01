dbde56b7e93d5cb8ef99dd7c4c8650852d8a07e6
sutanervin-dev <sutanervinlorenza@gmail.com>
2021-02-02 00:05:35 +0700

    fix width card jadi auto, tadi lupa masih 18rem

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ class ProductCard extends React.Component {
     const { product } = this.props
     return (
       <div className="col-3 mt-3">
-        <div className="card border-light shadow h-100" style={{ width: '18rem' }}>
+        <div className="card border-light shadow h-100" style={{ width: 'auto' }}>
             <div className="card-body">
             <img src={ product.image } className="card-img-top" alt={ product.title } style={{ width: '100%', height: '40vh', objectFit: 'contain' }} />
                     <h5 className="card-title text-center">{ product.title }</h5>
